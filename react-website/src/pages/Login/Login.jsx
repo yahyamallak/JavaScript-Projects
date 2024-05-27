23d6f532538c006feda1549fa672d5bb08ccd91b
Yahya mallak <Yahya10mallak@gmail.com>
2024-05-27 19:54:03 +0100

    add login register design

--- react-website/src/pages/Login/Login.jsx
+++ react-website/src/pages/Login/Login.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Login.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login-section'>
+      <h2 className='title'><FontAwesomeIcon icon="fa-solid fa-right-to-bracket" /> Login</h2>
+      <input type="text" name='email' placeholder='Email...' />
+      <input type="password" name='password' placeholder='Password...' />
+      <button><FontAwesomeIcon icon="fa-solid fa-right-to-bracket" /> Login</button>
+    </div>
   )
 }
 
--- react-website/src/pages/Register/Register.jsx
+++ react-website/src/pages/Register/Register.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='register-section'>
+      <h2 className='title'><FontAwesomeIcon icon="fa-solid fa-user-plus" /> Register</h2>
+      <input type="text" name='name' placeholder='Full name...' />
+      <input type="text" name='email' placeholder='Email...' />
+      <input type="password" name='password' placeholder='Password...' />
+      <input type="password" name='password_confirmation' placeholder='Confirm password...' />
+      <button><FontAwesomeIcon icon="fa-solid fa-user-plus" /> Register</button>
+    </div>
   )
 }
 
